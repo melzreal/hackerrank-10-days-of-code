cee5c4c29f5d5117efe12d213980d8884ce7b57e
melzreal <melissa.real@hotmail.com>
2020-02-23 20:00:54 +0000

    days 3 and 4

--- day3.js
+++ day3.js
@@ -1,4 +1,65 @@
-//problem:
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+
+function getSecondLargest(nums) {
+    //we need to iterate through each number and order them
+    //then find the largest
+    //then go through all the other numbers and return the largest number not equal to 
+    //the stored one. Like a bubble sort where the second one gets stored in a variable
 
 
-//solution
+    let largestNum = nums[0];
+    let secondLargest = nums[0];
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > largestNum) {
+            secondLargest = largestNum;
+            largestNum = nums[i];
+
+        }
+        if ((nums[i] > secondLargest) && (nums[i] < largestNum)) {
+            secondLargest = nums[i];
+        }
+    }
+
+
+    return secondLargest;
+}
+
+
+
+// // day 3, try catch. 
+// Try to reverse string  using the split, reverse, and join methods.
+// If an exception is thrown, catch it and print the contents of the exception's  on a new line.
+// Print  on a new line. If no exception was thrown, then this should be the reversed string; if an exception was thrown, this should be the original string.
+
+function reverseString(s) {
+    try {
+        console.log(s.split('').reverse().join(''));
+    } catch (e) {
+        console.log(e.message, "\n" + s);
+    }
+}
+
+
+//throw, day 3
+function isPositive(a) {
+
+    if (a === 0) {
+        throw new Error('Zero Error');
+
+    } else if (a < 0) {
+        throw new Error('Negative Error');
+
+    } else {
+        return 'YES';
+    }
+
+
+
+}
+
--- day4.js
+++ day4.js
@@ -1,4 +1,65 @@
-//problem:
+/*
+ * Complete the Rectangle function
+ */
+function Rectangle(a, b) {
+
+    this.length = a;
+    this.width = b;
+    this.perimeter = 2 * (a + b);
+    this.area = a * b;
+
+}
 
 
-//solution
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ * 
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+function getCount(objects) {
+
+    let counter = 0;
+
+    objects.forEach(key => {
+        if (key.x === key.y) {
+            counter++;
+        }
+    });
+
+    return counter;
+
+
+
+}
+
+
+/*
+ * Implement a Polygon class with the following properties:
+ * 1. A constructor that takes an array of integer side lengths.
+ * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
+ */
+class Polygon {
+    constructor(length) {
+        this.length = length;
+
+    }
+
+    perimeter() {
+        const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+        return this.length.reduce(reducer);
+    }
+}
+
+
+
+class Rectangle {
+    constructor(w, h) {
+        this.w = w;
+        this.h = h;
+    }
+}
+
+
+
